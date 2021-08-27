58f6e2d2f93b96854545826942aea075147b0f6f
primetzvan <vaniprimetz@gmail.com>
2021-08-27 12:55:43 +0200

    fix: started database after reboo - start.sh

--- config/newcluster/start.sh
+++ config/newcluster/start.sh
@@ -10,6 +10,11 @@ sudo chmod +x startdb.sh
 
 sudo ./startdb.sh
 
+sudo echo "galera_new_cluster" > /etc/init.d/StartDatabase.sh
+ 
+sudo chmod 755 /etc/init.d/StartDatabase.sh
+sudo update-rc.d /etc/init.d/StartDatabase.sh defaults
+
 sudo chmod +x kioskmode.sh
 
 sudo ./kioskmode.sh
--- config/newnode/start.sh
+++ config/newnode/start.sh
@@ -10,6 +10,11 @@ sudo chmod +x newnodesetup.sh
 
 sudo ./newnodesetup.sh
 
+sudo echo "sudo systemctl start mysql" > /etc/init.d/StartDatabase.sh
+ 
+sudo chmod 755 /etc/init.d/StartDatabase.sh
+sudo update-rc.d /etc/init.d/StartDatabase.sh defaults
+
 sudo chmod +x kioskmode.sh
 
 sudo ./kioskmode.sh
