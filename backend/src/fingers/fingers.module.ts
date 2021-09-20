e98671c4b7ad1d8ed9ef10c6b75f628fa66f5926
FeichtingerSarah <feichtinger.sarah5@gmail.com>
2021-09-20 07:48:57 +0200

    feat: registert mq client

--- backend/src/fingers/fingers.module.ts
+++ backend/src/fingers/fingers.module.ts
@@ -4,9 +4,21 @@ import { Finger } from './entities/finger.entity';
 import { FingersService } from './fingers.service';
 import { FingersController } from './fingers.controller';
 import { User } from '../users/entities/user.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Finger, User])],
+  imports: [
+    TypeOrmModule.forFeature([Finger, User]),
+    ClientsModule.register([
+      {
+        name: 'MQ_CLIENT',
+        transport: Transport.MQTT,
+        options: {
+          url: 'mqtt://localhost:1883'
+        },
+      },
+    ]),
+  ],
   providers: [FingersService],
   controllers: [FingersController],
   exports: [FingersService],
